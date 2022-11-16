5218d983d57ae210668096a9f96ef31f340038d6
thascoet <thascoet3@gmail.com>
2022-11-16 18:39:33 +0000

    connect db

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const { logger } = require('./middlewares/logs');
+const { logger, logEvents } = require('./middlewares/logs');
 const errorHandler = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT;
@@ -46,8 +47,27 @@ app.all('*', (req, res) => {
 app.use(errorHandler);
 
 
+// connect to database
+
+const connectDB = async () => {
+  try {
+      await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+  } catch (err) {
+      console.log(err)
+  }
+}
+
+connectDB();
+
+
 // Start the api
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => console.log(`App listening on port ${port}`));
+});
+
+mongoose.connection.on('error', err => {
+  console.log(err);
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+});
