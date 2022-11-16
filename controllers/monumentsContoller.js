62350052d5e6191f01202781f39820d411b82e9d
thascoet <thascoet3@gmail.com>
2022-11-16 19:22:29 +0000

    global architecture

---
+++ controllers/monumentsContoller.js
@@ -0,0 +1,30 @@
+const Monument = require('./../models/Monument');
+const asyncHandler = require('express-async-handler');
+
+const getAllMonuments = asyncHandler(async (req, res) => {
+
+});
+
+const getMonumentById = asyncHandler(async (req, res) => {
+
+});
+
+const createNewMonument = asyncHandler(async (req, res) => {
+
+});
+
+const updateMonumentById = asyncHandler(async (req, res) => {
+
+});
+
+const deleteMonumentById = asyncHandler(async (req, res) => {
+
+});
+
+module.exports = {
+    getAllMonuments,
+    getMonumentById,
+    createNewMonument,
+    updateMonumentById,
+    deleteMonumentById
+}
